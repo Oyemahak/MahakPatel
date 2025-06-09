ce0012e042de12b230d29757d2299cd271e4ba22
Mahak Patel <mahakpateluiux@gmail.com>
2025-06-09 06:12:04 -0400

    Fixed local setup and restored from Trash - Ready for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // or just remove it — Vercel handles routing perfectly
+  base: '/', // Works best for root deployments on Vercel
 });
